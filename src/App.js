6b7d535ff878bd054ba184b272c41a8785d48021
kangkyeongseo <kks_big@naver.com>
2023-01-10 14:01:07 +0900

    useFadeIn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-const usePreventLeave = () => {
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = "";
-  };
-  const enablePrevent = () => window.addEventListener("beforeunload", listener);
-  const disablePrevent = () =>
-    window.removeEventListener("beforeunload".listener);
-  return { enablePrevent, disablePrevent };
+const useFadeIn = (duration = 1) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof duration !== "number") {
+      return;
+    }
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const { enablePrevent, disablePrevent } = usePreventLeave();
+  const fadeInH1 = useFadeIn();
+  const fadeInP = useFadeIn(10);
   return (
     <div>
-      <h1>usePreventLeave</h1>
-      <button onClick={enablePrevent}>Protect</button>
-      <button onClick={disablePrevent}>Unprotect</button>
+      <h1 {...fadeInH1}>useFadeIn</h1>
+      <p {...fadeInP}>content</p>
     </div>
   );
 };
